d9c4e2424cbfc10abd8923a90a830032bcb2dabc
Fredrik Wärnsberg <fredrik@warnsberg.se>
2017-08-29 13:59:10 +0200

    Updated mixfile

--- mix.exs
+++ mix.exs
@@ -5,9 +5,13 @@ defmodule AbsintheMetrics.Mixfile do
     [
       app: :absinthe_metrics,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.4",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      name: "absinthe_metrics",
+      source_url: "https://github.com/elixir-ecto/postgrex"
     ]
   end
 
@@ -23,6 +27,21 @@ defmodule AbsintheMetrics.Mixfile do
     [
       {:absinthe, "~> 1.3"},
       {:prometheus_ex, "~> 1.4", optional: true},
+      {:ex_doc, ">= 0.0.0", only: :dev},
+    ]
+  end
+
+  defp description do
+    "Pluggable metrics for Absinthe GraphQL."
+  end
+
+  defp package do
+    [
+      name: "absinthe_metrics",
+      licenses: ["MIT"],
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Fredrik Wärnsberg"],
+      links: %{"GitHub" => "https://github.com/soundtrackyourbrand/absinthe-metrics"}
     ]
   end
 end
