5ee19e45046067ba5b8448fe01d27f1073093d0a
Fredrik Wärnsberg <fredrik@warnsberg.se>
2017-09-20 15:08:45 +0200

    Refactor PrometheusHistogram to use labels

--- lib/backends/prometheus_histogram.ex
+++ lib/backends/prometheus_histogram.ex
@@ -1,16 +1,28 @@
 defmodule AbsintheMetrics.Backend.PrometheusHistogram do
   @behaviour AbsintheMetrics
+  @query_metric_name :graphql_query_duration_microseconds
+  @field_metric_name :graphql_fields_duration_microseconds
   use Prometheus
 
-  def field(object, field, buckets: buckets) do
-    true = Histogram.declare([name: to_metric(object, field),
-                              labels: [:status],
-                              buckets: buckets,
-                              help: "Metrics for GraphQL resolver: #{to_metric(object, field)}"])
+  def field(:query, _field, buckets: buckets) do
+    _ = Histogram.declare([name: @query_metric_name,
+                           labels: [:query, :status],
+                           buckets: buckets,
+                           help: "Resolution time for GraphQL queries"])
   end
 
-  def instrument(object, field, {status, _}, time), do: Histogram.observe([name: to_metric(object, field), labels: [status]], time)
+  def field(object, field, buckets: buckets) do
+    _ = Histogram.declare([name: @field_metric_name,
+                           labels: [:object, :field, :status],
+                           buckets: buckets,
+                           help: "Metrics for GraphQL field resolvers"])
+  end
 
-  defp to_metric(:query, field), do: "#{field}_duration_microseconds"
-  defp to_metric(object, field), do: "#{object}_#{field}_duration_microseconds" |> String.to_atom()
+  def instrument(:query, field, {status, _}, time) do
+    Histogram.observe([name: @query_metric_name, labels: [field, status]], time)
+  end
+
+  def instrument(object, field, {status, _}, time) do
+    Histogram.observe([name: @field_metric_name, labels: [object, field, status]], time)
+  end
 end
