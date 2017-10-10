ce811ec728989bdfc73ff5bd4e8819edc8778635
Fredrik Wärnsberg <fredrik@warnsberg.se>
2017-10-10 17:20:47 +0200

    0.6.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AbsintheMetrics.Mixfile do
   def project do
     [
       app: :absinthe_metrics,
-      version: "0.5.0",
+      version: "0.6.0",
       elixir: "~> 1.4",
       start_permanent: Mix.env == :prod,
       deps: deps(),
